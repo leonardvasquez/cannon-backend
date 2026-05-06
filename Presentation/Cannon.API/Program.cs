6663be58e4c8dd06a9502ca3c3e646ee012d1d13
Leonard Vasquez <leonardvasquez22@gmail.com>
2026-05-06 16:51:28 -0500

    Se configura CORS para permitir consumo desde frontend Angular

--- Presentation/Cannon.API/Program.cs
+++ Presentation/Cannon.API/Program.cs
@@ -9,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -26,5 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 app.MapControllers();
 app.Run();
